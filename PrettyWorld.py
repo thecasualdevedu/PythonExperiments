67438df7f3b23359358f355a35ba486bb8020085
Jack <s202023145@my.chisholm.edu.au>
2024-04-15 14:30:49 +1000

    Created the worst looking hello world !!

---
+++ PrettyWorld.py
@@ -0,0 +1,18 @@
+from colorama import Fore, Style
+
+print (
+    Style.BRIGHT +
+    Fore.RED + "H" +
+    Fore.YELLOW + "e" +
+    Fore.CYAN + "l" +
+    Fore.GREEN + "l" +
+    Fore.MAGENTA + "o",
+
+    Style.BRIGHT +
+    Fore.RED + "W" +
+    Fore.YELLOW + "o" +
+    Fore.CYAN + "r" +
+    Fore.GREEN + "l" +
+    Fore.MAGENTA + "d" +
+    Fore.LIGHTYELLOW_EX + "!",
+)
