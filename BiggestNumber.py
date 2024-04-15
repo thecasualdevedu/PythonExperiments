9d985251df7006f28412f5adce1b0ed678df2665
Jack <s202023145@my.chisholm.edu.au>
2024-04-15 15:31:49 +1000

    Updated the BiggsetNumber with a new comment

--- BiggestNumber.py
+++ BiggestNumber.py
@@ -5,6 +5,7 @@ os.system("cls")
 
 def main () :
 
+    # setups the values
     rawValue = input("Input Value: ")
     values = list(map(int, rawValue.split( )))
 
@@ -12,11 +13,14 @@ def main () :
 
     # print (values) -- used for testing the value
 
+    # gets the values list, and cycles through it
     for value in values :
-
+        
+        # if the new value is bigger then the last set value it will update the big_num value
         if value > big_num :
             big_num = value
     
+    # print results
     print (
         "The biggest number of the", len (values), "numbers is:", big_num)
 
