970d060f619f818178ea50edfbd43b17dbc6c46b
Jack W112 <s202023145@my.chisholm.edu.au>
2024-04-29 14:22:30 +1000

    fixed

--- Week 3 - Loops/counting.py
+++ Week 3 - Loops/counting.py
@@ -1,5 +1,6 @@
-number:int = int(input("How high do you wish to count too? : "))
+goal:int = int(input("How high do you wish to count too? : "))
+number:int = 1
 
-while number <= 10:
+while number <= goal:
     print(number)
     number += 1
